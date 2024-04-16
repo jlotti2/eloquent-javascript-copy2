4b088066643083ff7f0dabaa338bb0628bd2f078
jlotti2 <jlottinger2@gmail.com>
2024-04-16 15:57:31 -0500

    chapter5 complete

--- chapter-05/exercises.js
+++ chapter-05/exercises.js
@@ -2,7 +2,10 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
+function flatten(arrays) {
+  return arrays.reduce(function(accumulator, currentArray) {
+    return accumulator.concat(currentArray);
+  }, []);
 
 }
 
@@ -10,7 +13,12 @@ function flatten() {
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
+function loop(value, test, update, body) {
+  // Start with the initial value and continue the loop until the test function returns false
+  for (let currentValue = value; test(currentValue); currentValue = update(currentValue)) {
+    // Call the body function with the current value
+    body(currentValue);
+  }
 
 }
 
@@ -18,17 +26,52 @@ function loop() {
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
+function every(array, test) {
+  for (let element of array) {
+    if (!test(element)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function dominantDirection(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
 
-}
+  if (scripts.length === 0) return "No dominant direction found";
+
+  return scripts.reduce((a, b) => a.count > b.count ? a : b).name;
+};
+  function characterScript(code) {
+    for (let script of SCRIPTS) {
+      if (script.ranges.some(([from, to]) => code >= from && code < to)) {
+        return script;
+      }
+    }
+    return null;
+  };
+  
+  function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+      let name = groupName(item);
+      let known = counts.find(c => c.name == name);
+      if (!known) {
+        counts.push({name, count: 1});
+      } else {
+        known.count++;
+      }
+    }
+    return counts;
+  }
+
 
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
